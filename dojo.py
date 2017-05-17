72d301809868665932901cf009cf3f18dffe886d
Wambui Sharon <wambuisha50@gmail.com>
2017-05-17 13:01:58 -0400

    Implemented PEP8 requirements

--- dojo.py
+++ dojo.py
@@ -2,8 +2,9 @@ import random
 from room import Office, Living_space
 from person import Fellow, Staff
 
+
 class Dojo(object):
-    #Constructor
+    # Constructor
     def __init__(self):
         self.list_of_offices = []
         self.list_of_living_space = []
@@ -11,7 +12,7 @@ class Dojo(object):
         self.list_of_staff = []
         self.list_of_occupants = []
 
-    def create_room(self,room_name,room_type):
+    def create_room(self, room_name, room_type):
         all_rooms = self.list_of_living_space + self.list_of_offices
         all_room_names = []
         for room in all_rooms:
@@ -22,39 +23,36 @@ class Dojo(object):
             if room_type == "Office":
                 new_office = Office(room_name)
                 self.list_of_offices.append(new_office)
-               
                 return new_office
 
             elif room_type == "Living_space":
-                new_living_space= Living_space(room_name)
+                new_living_space = Living_space(room_name)
                 self.list_of_living_space.append(new_living_space)
-               
+
                 return new_living_space
 
         elif room_name in all_room_names:
-            
-            return "Sorry. Room %s already exists"%room_name
+            return "Sorry. Room %s already exists" % room_name
 
-
-
-    def add_person(self,person_name,person_type, accommodation="No"):
+    def add_person(self, person_name, person_type, accommodation="No"):
         if person_type == "Staff":
             new_staff = Staff(person_name)
             self.list_of_staff.append(new_staff)
-            # get a random room 
+            # get a random room
             random_room = random.choice(self.list_of_offices)
             # add member to the occupants of the selected room
             random_room.occupants.append(new_staff)
-            
+
             return new_staff
 
         elif person_type == "Fellow":
             new_fellow = Fellow(person_name)
-            self.list_of_fellows.append(new_fellow) 
-            print("This fellow %s has been added successfully" % new_fellow.person_name)
+            self.list_of_fellows.append(new_fellow)
+            print("This fellow %s has been added successfully"
+                  % new_fellow.person_name)
             random_room = random.choice(self.list_of_offices)
             random_room.occupants.append(new_fellow)
-            #Add a living room to a fellow
+            # Add a living room to a fellow
             if accommodation == "yes":
                 random_room = random.choice(self.list_of_living_space)
                 random_room.occupants.append(new_fellow)
@@ -62,5 +60,5 @@ class Dojo(object):
             return new_fellow
 
         else:
-            
+
             return "We ran into an error"
