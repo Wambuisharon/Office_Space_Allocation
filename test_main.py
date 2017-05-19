80d76aa37a86dc10bbb6cc517c1f94e9014cf3e8
Wambui Sharon <wambuisha50@gmail.com>
2017-05-19 00:10:32 -0400

    Refactored test

--- test_main.py
+++ test_main.py
@@ -7,16 +7,16 @@ class TestCreateRoom(unittest.TestCase):
 
     def setUp(self):
         self.dojo = Dojo()
-        self.dojo.create_room("Spire", "Office")
-        self.dojo.create_room("Tent", "Living_space")
+        self.dojo.create_room("Spire", "OFFICE")
+        self.dojo.create_room("Tent", "LIVING_SPACE")
 
     # Create office test
     def test_create_office_successfully(self):
-        test_create_office = self.dojo.create_room("Lion", "Office")
+        test_create_office = self.dojo.create_room("LION", "OFFICE")
         self.assertIn(test_create_office, self.dojo.list_of_offices)
 
     def test_office_allocation(self):
-        test_office_allocation = self.dojo.add_person("John Doe", "Fellow")
+        test_office_allocation = self.dojo.add_person("John Doe", "FELLOW")
         all_occupants = []
         for room in self.dojo.list_of_offices:
             for person in room.occupants:
@@ -26,41 +26,42 @@ class TestCreateRoom(unittest.TestCase):
 
     def test_create_living_space_successfully(self):
         test_create_living_space = \
-            self.dojo.create_room("Mt_Kenya", "Living_space")
+            self.dojo.create_room("Mt_Kenya", "LIVING_SPACE")
         self.assertIn(test_create_living_space, self.dojo.list_of_living_space)
 
     def test_add_fellow(self):
-        test_add_fellow = self.dojo.add_person("Washera", "Fellow")
+        test_add_fellow = self.dojo.add_person("Washera", "FELLOW")
         self.assertIn(test_add_fellow, self.dojo.list_of_fellows)
 
     def test_add_staff(self):
-        test_add_staff = self.dojo.add_person("Office", "Staff")
+        test_add_staff = self.dojo.add_person("Dudhd", "STAFF")
         self.assertIn(test_add_staff, self.dojo.list_of_staff)
 
     def test_office_with_similar_names(self):
         test_office_with_similar_names = self.dojo.create_room("Spire",
-                                                               "Office")
+                                                               "OFFICE")
         self.assertEquals(test_office_with_similar_names,
                           "Sorry. Room Spire already exists")
 
     def test_living_room_with_similar_names(self):
         test_living_room_with_similar_names = \
-            self.dojo.create_room("Tent", "Living_space")
+            self.dojo.create_room("Tent", "LIVING_SPACE")
         self.assertEquals(test_living_room_with_similar_names,
                           "Sorry. Room Tent already exists")
 
     def test_living_space_allocation(self):
         test_living_space_allocation = \
-            self.dojo.add_person("John Doe1", "Fellow", accommodation="yes")
+            self.dojo.add_person("John Doe1", "FELLOW", accommodation="YES")
         all_occupants = []
         for room in self.dojo.list_of_living_space:
             for person in room.occupants:
                 all_occupants.append(person)
-        self.assertIn(test_living_space_allocation, all_occupants)
+        self.assertIn(test_living_space_allocation,
+                      "Fellow John Doe1 has been allocated Tent")
 
     def test_create_living_space_with_an_exist_office_name(self):
-        self.dojo.create_room("Tsavo", "Office")
-        new_tsavo = self.dojo.create_room("Tsavo", "Living_space")
+        self.dojo.create_room("Tsavo", "OFFICE")
+        new_tsavo = self.dojo.create_room("Tsavo", "LIVING_SPACE")
         self.assertEquals("Sorry. Room Tsavo already exists", new_tsavo)
 
 
